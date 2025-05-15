6d06010e8ab4c2917626ee99d33c3d2f8eda5811
mxm137 <204676343@qq.com>
2025-05-15 18:54:26 +0800

    Update Navigator.js
    2025-5-15 18:54:23

--- ui/src/pages/Navigator.js
+++ ui/src/pages/Navigator.js
@@ -27,9 +27,7 @@ export default function Navigator({initialized, token, localChanged}) {
     const r0 = `${location.pathname}${location.search}`;
     setNavs([
       {eventKey: '2', to: '/routers-scenario', text: t('nav.scenario')},
-      {eventKey: '3', to: '/routers-settings', text: t('nav.system')},
-      {eventKey: '4', to: '/routers-components', text: t('nav.component')},
-      {eventKey: '5', to: '/routers-contact', text: t('nav.contact')},
+      {eventKey: '3', to: '/routers-settings', text: t('nav.system')},      
       {eventKey: '6', to: '/routers-logout', text: t('nav.logout')},
     ].map(e => {
       if (r0.indexOf(e.to) >= 0) {
